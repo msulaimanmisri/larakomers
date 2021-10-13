b4168731007e996656c860bf651b0daf8758288e
Sulaiman Misri <msulaimanmisri@gmail.com>
2021-10-13 22:35:43 +0800

    Create Login Form and Set new route for dashboard

--- resources/views/layouts/footer.blade.php
+++
@@ -1,7 +0,0 @@
-{{-- Base JS --}}
-<script src="{{ asset('js/app.js') }}"></script>
-<script src="{{ asset('assets/plugins/feather-icons/feather.min.js') }}"></script>
-<script src="{{ asset('assets/plugins/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
-
-{{-- Plugin JS --}}
-<script src="{{ asset('assets/js/template.js') }}"></script>
