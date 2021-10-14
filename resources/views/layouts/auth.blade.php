e57c8832a005212fc41e027c8dae4d9d8692ab7d
Sulaiman Misri <msulaimanmisri@gmail.com>
2021-10-14 22:12:44 +0800

    Create Reset Password View

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -14,10 +14,13 @@
     <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}">
 
     {{-- CSS --}}
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+
 </head>
 
-<body>
+<body class="bg-light">
     @yield('content')
     @include('layouts.auth-footer')
 </body>
