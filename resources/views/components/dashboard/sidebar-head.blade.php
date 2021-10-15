3ce543a4e8110f856c6f931912ac6c48b39162ee
Sulaiman Misri <msulaimanmisri@gmail.com>
2021-10-15 23:23:20 +0800

    Add Sidebar

---
+++ resources/views/components/dashboard/sidebar-head.blade.php
@@ -0,0 +1,8 @@
+<a href="{{ route('admin.dashboard.index') }}" class="sidebar-brand">
+    Lara<span>Komers</span>
+</a>
+<div class="sidebar-toggler not-active">
+    <span></span>
+    <span></span>
+    <span></span>
+</div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,11 +12,10 @@
 
     {{-- Icon --}}
     <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.0/font/bootstrap-icons.css">
 
     {{-- CSS --}}
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
-
 
 </head>
 
