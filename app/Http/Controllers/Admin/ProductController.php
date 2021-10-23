da368ade5901fd6bbfff62985f6ff2dad1bdf618
Sulaiman Misri <msulaimanmisri@gmail.com>
2021-10-23 14:50:59 +0800

    Product : Add Create Method & View

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Models\Product;
+use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -26,7 +27,8 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
+        $productCategory = Category::latest()->get();
+        return view('product.create', compact($productCategory));
     }
 
     /**
