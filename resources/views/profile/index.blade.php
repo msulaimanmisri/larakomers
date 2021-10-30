bf9763ce5aaf9f11659e5cc2536b81ae91e44d63
Sulaiman Misri <msulaimanmisri@gmail.com>
2021-10-30 23:37:30 +0800

    Profile : Create & Add Two-factor and Profile

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -1,4 +1,8 @@
 @extends('layouts.app')
 @section('content')
+
 @include('components.profile.status')
+@include('components.profile.two-factor')
+@include('components.profile.profile')
+
 @endsection
